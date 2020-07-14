9b14a32791adcb3fb89c397bc068de0bca31a2c2
ho <whdxo10@naver.com>
2020-07-14 21:27:45 +0900

    07/14 APP: POST COMPONENT - Post Component Swiper
    	1. Swip install
    	yarn add react-native-swiper@nightly
    	
    	2. Swip 이용하여 photo swip 구현
    	
    	3. Photo 하단에 좋아요/커맨트 등 아이콘 추가
    	- Iconview -> iconcontainer -> 모든 아이콘은 touchable로 감싸야함.

--- components/Post.js
+++ components/Post.js
@@ -1,74 +1,111 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
-import { Image } from "react-native";
+import { Image, StyleSheet } from "react-native";
+import Swiper from "react-native-swiper";
+import constants from "../constants";
+import { Ionicons } from "@expo/vector-icons";
 
 const Container = styled.View``;
 const Header = styled.View`
-    padding:20px;
-    flex-direction:row;
-    align-items:center;
+  padding: 20px;
+  flex-direction: row;
+  align-items: center;
 `;
 const Touchable = styled.TouchableOpacity``;
 
 const HeaderUserContainer = styled.View`
-    margin-left:10px;
+  margin-left: 10px;
 `;
 
 const Bold = styled.Text`
-    font-weight:500;
+  font-weight: 500;
 `;
 
-const Location = styled.Text`    
-    font-size:12px;
+const Location = styled.Text`
+  font-size: 12px;
 `;
 
-const Post = ({user, location}) => {
-    return(
-        <Container>
-            <Header>
-                <Touchable>
-                    <Image style={{height:40, width:40, borderRadius:20}} source = {{uri: user.avatar}}/>
-                </Touchable>
-                <Touchable>
-                    <HeaderUserContainer>
-                        <Bold>{user.username}</Bold>
-                        <Location>{location}</Location>
-                    </HeaderUserContainer>
-                </Touchable>
-            </Header>
-        </Container>
-    )
-}
+const IconsContainer = styled.View`
+  padding: 15px;
+  flex-direction:row;
+`;
+
+const IconView = styled.View`
+    margin-right:10px;
+`;
+
+const Post = ({ user, location, files = [] }) => {
+  return (
+    <Container>
+      <Header>
+        <Touchable>
+          <Image
+            style={{ height: 40, width: 40, borderRadius: 20 }}
+            source={{ uri: user.avatar }}
+          />
+        </Touchable>
+        <Touchable>
+          <HeaderUserContainer>
+            <Bold>{user.username}</Bold>
+            <Location>{location}</Location>
+          </HeaderUserContainer>
+        </Touchable>
+      </Header>
+      <Swiper showsButtons={true} style={{ height: constants.height / 2.5 }}>
+        {files &&
+          files.map((file) => (
+            <Image
+              key={file.id}
+              style={{ height: constants.height / 2.5, width: constants.width }}
+              source={{ uri: file.url }}
+            />
+          ))}
+      </Swiper>
+      <IconsContainer>
+        <IconView>
+          <Touchable>
+            <Ionicons name={"md-heart-empty"} size={30} />
+          </Touchable>
+        </IconView>
+        <IconView>
+          <Touchable>
+            <Ionicons name={"md-message-empty"} size={30} />
+          </Touchable>
+        </IconView>
+      </IconsContainer>
+    </Container>
+  );
+};
 
 Post.propTypes = {
+  id: PropTypes.string.isRequired,
+  user: PropTypes.shape({
     id: PropTypes.string.isRequired,
-    user: PropTypes.shape({
+    avatar: PropTypes.string,
+    username: PropTypes.string.isRequired,
+  }).isRequired,
+  files: PropTypes.arrayOf(
+    PropTypes.shape({
       id: PropTypes.string.isRequired,
-      avatar: PropTypes.string,
-      username: PropTypes.string.isRequired,
-    }).isRequired,
-    files: PropTypes.arrayOf(
-      PropTypes.shape({
+      url: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  likeCount: PropTypes.number.isRequired,
+  isLiked: PropTypes.bool.isRequired,
+  comments: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      text: PropTypes.string.isRequired,
+      user: PropTypes.shape({
         id: PropTypes.string.isRequired,
-        url: PropTypes.string.isRequired,
-      })
-    ).isRequired,
-    likeCount: PropTypes.number.isRequired,
-    isLiked: PropTypes.bool.isRequired,
-    comments: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        text: PropTypes.string.isRequired,
-        user: PropTypes.shape({
-          id: PropTypes.string.isRequired,
-          username: PropTypes.string.isRequired,
-        }).isRequired,
-      })
-    ).isRequired,
-    createdAt: PropTypes.string.isRequired,
-    caption: PropTypes.string.isRequired,
-    location: PropTypes.string,
+        username: PropTypes.string.isRequired,
+      }).isRequired,
+    })
+  ).isRequired,
+  createdAt: PropTypes.string.isRequired,
+  caption: PropTypes.string.isRequired,
+  location: PropTypes.string,
 };
 
 export default Post;
