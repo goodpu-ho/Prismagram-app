e4ad7f4a97a63317f7b0332f14e5da0809fc14bd
ho <whdxo10@naver.com>
2020-07-25 20:03:16 +0900

    07/25 APP: UPLOAD - Take Photo part One 
    	1. Select photo grid 형식으로 변경하고, selct한 photo border color변경
    	2. takePhoto 작업
    	• Expo-camera를 이용하여 camera show 까지만 진행.
    	• Permission 등등 은 select photo와 동일 로직

--- screens/Photo/SelectPhoto.js
+++ screens/Photo/SelectPhoto.js
@@ -3,14 +3,28 @@ import styled from "styled-components";
 import * as Permissions from "expo-permissions";
 import * as MediaLibrary from "expo-media-library";
 import Loader from "../../components/Loader";
-import { Image, ScrollView } from "react-native";
+import { Image, ScrollView, StyleSheet } from "react-native";
 import constants from "../../constants";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import styles from "../../styles";
 
 const View = styled.View`
   flex: 1;
 `;
 
 const Text = styled.Text``;
+
+const GridView = styled.View``;
+
+const grid = StyleSheet.create({
+  list: { flex: 1, width: "100%" },
+  stylegridView: {
+    flexDirection: "row",
+    flexWrap: "wrap",    
+    paddingTop: 10,
+    justifyContent: "space-between",
+  },
+});
 
 export default () => {
   const [loading, setLoading] = useState(true);
@@ -49,6 +63,10 @@ export default () => {
     askPermission();
   }, []);
 
+  const changeSelect = (photo) => {
+    setSelected(photo);
+  };
+
   return (
     <View>
       {loading ? (
@@ -56,16 +74,46 @@ export default () => {
       ) : (
         <View>
           {permission ? (
-            <ScrollView contentContainerStyle={{flexDirection:"row"}}>
-              {allPhotos &&
-                allPhotos.map((photo) => (
-                  <Image
-                    key={photo.id}
-                    source={{ uri: photo.uri }}
-                    style={{ width: constants.width/3, height: constants.height/5 }}
-                  />
-                ))}
-            </ScrollView>
+            <>
+              <Image
+                source={{ uri: selected.uri }}
+                resizeMode="contain"
+                style={{
+                  width: constants.width,
+                  height: constants.height / 3,
+                  borderTopLeftRadius: 50,
+                  borderTopRightRadius: 50,
+                  borderBottomLeftRadius: 50,
+                  borderBottomRightRadius: 50,
+                  borderColor: styles.blueColor,
+                }}
+              />
+              <ScrollView style={grid.list}>
+                <GridView style={grid.stylegridView}>
+                  {allPhotos &&
+                    allPhotos.map((photo) => (
+                      <TouchableOpacity
+                        key={photo.id}
+                        onPress={() => changeSelect(photo)}
+                      >
+                        <Image
+                          source={{ uri: photo.uri }}
+                          style={{
+                            width: constants.width / 3,
+                            height: constants.height / 5,
+                            opacity: photo.id === selected.id ? 0.5 : 1,
+                            borderWidth: photo.id === selected.id ? 2 : 0,
+                            borderColor:
+                              photo.id === selected.id
+                                ? styles.redColor
+                                : styles.blackColor,
+                          }}
+                        />
+                      </TouchableOpacity>
+                    ))}
+                </GridView>
+              </ScrollView>
+            </>
           ) : null}
         </View>
       )}
--- screens/Photo/TakePhoto.js
+++ screens/Photo/TakePhoto.js
@@ -1,19 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { TouchableOpacity } from "react-native";
 import styled from "styled-components";
+import { Camera } from "expo-camera";
 
 const View = styled.View`
-  justify-content: center;
-  align-items: center;
   flex: 1;
 `;
 
 const Text = styled.Text``;
 
-export default ({ navigation }) => (
-  <View>
-    <TouchableOpacity onPress={() => navigation.navigate("UploadPhoto")}>
-      <Text>Take</Text>
-    </TouchableOpacity>
-  </View>
-);
+export default ({ navigation }) => {
+  const [loading, setLoading] = useState(true);
+  const [permission, setPermission] = useState(false);
+
+  const askPermission = async () => {
+    try {
+      const { status } = await Camera.requestPermissionsAsync();
+      if (status === "granted") {
+        setPermission(true);
+      }
+    } catch {}
+  };
+
+  useEffect(() => {
+    askPermission();
+  }, []);
+
+  return (
+    <View>
+      <Camera style={{ flex: 1 }}>
+
+      </Camera>
+    </View>
+  );
+};
