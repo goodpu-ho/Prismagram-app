3774ddcb55dddd07a0f2408672a05747e46fbb3e
ho <whdxo10@naver.com>
2020-07-25 22:36:59 +0900

    07/25 APP: UPLOAD - Take Photo part Two 
    	• 카메라 front, back type 바꾸는 로직 및 버튼 추가

--- screens/Photo/TakePhoto.js
+++ screens/Photo/TakePhoto.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { TouchableOpacity } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 import styled from "styled-components";
 import { Camera } from "expo-camera";
+import constants from "../../constants";
+import { Ionicons } from "@expo/vector-icons";
 
 const View = styled.View`
   flex: 1;
@@ -12,6 +14,7 @@ const Text = styled.Text``;
 export default ({ navigation }) => {
   const [loading, setLoading] = useState(true);
   const [permission, setPermission] = useState(false);
+  const [cameraType, setCameraType] = useState(Camera.Constants.Type.back);
 
   const askPermission = async () => {
     try {
@@ -19,17 +22,37 @@ export default ({ navigation }) => {
       if (status === "granted") {
         setPermission(true);
       }
-    } catch {}
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   useEffect(() => {
     askPermission();
   }, []);
 
+  const toggleCameraType = () => {
+    if(cameraType === Camera.Constants.Type.back) {
+      setCameraType(Camera.Constants.Type.front);
+    } else {
+      setCameraType(Camera.Constants.Type.back);
+    }
+  }
+
   return (
     <View>
-      <Camera style={{ flex: 1 }}>
-
+      <Camera
+        type={cameraType}
+        style={{
+          width: constants.width,
+          height: constants.height / 2,
+          justifyContent:"flex-end",
+          padding:20
+        }}
+      >
+        <TouchableOpacity onPress={ () => toggleCameraType()}>
+          <Ionicons name="md-reverse-camera" size={40}/>
+        </TouchableOpacity>
       </Camera>
     </View>
   );
