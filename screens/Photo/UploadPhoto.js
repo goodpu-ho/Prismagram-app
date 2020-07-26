9010d58cd6235a091b1ac09c2d781feb03c27c10
ho <whdxo10@naver.com>
2020-07-26 16:20:16 +0900

    07/26 APP: UPLOAD - Preparing for Upload 
    	1. Upload.js 화면 간략하게 구성
    	• Container(flex-directoni:row) -> image -> form -> textinput (use hook)
    	• Add upload button -> onpress -> handleSubmmit
    	
    	2. Yarn add multer (파일 업로드위해) - backend임
    	• File 업로드는 graphql을 사용할 수도 있느나 REST 방식을 쓴다. 그 이유는 graphql은 데이터 요청등에 최적화되어 있고, 리소스 측면에는 REST가 우위.
    	• Controller는 req.file을 가져옴(이 file은 middleware로 부터 온것) 
    	• Middleware가 실행하는 것이 바로 multer다.
    	• Multer가 하는 일은 어떤 사용자로부터 제출된 form을 받았을 때, 전달받은 파일을 upload해주는 것
    	• 따라서 우리는 업로드 과정을 하나하나 다룰 필요가 없다. 이름 짓고 url 할당하고 고유 이름주고 이런 것들을 multer가 해준다.

--- screens/Photo/UploadPhoto.js
+++ screens/Photo/UploadPhoto.js
@@ -1,21 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import { Image, Alert } from "react-native";
+import useInput from "../../hooks/useInput";
+import styles from "../../styles";
+import constants from "../../constants";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const View = styled.View`
-  justify-content: center;
-  align-items: center;
   flex: 1;
 `;
 
-const Text = styled.Text``;
+const Container = styled.View`
+  flex-direction: row;
+  padding: 20px;
+`;
+
+const Form = styled.View`
+  justify-content:space-between;
+  margin:10px;
+
+`;
+
+const STextInput = styled.TextInput`
+  width: ${constants.width - 180};
+  border: 0px solid ${styles.lightGreyColor};
+  border-bottom-width: 5px;     
+`;
+
+const Button = styled.TouchableOpacity`
+  background-color:${styles.blueColor};
+  border-radius:4px;
+  justify-content:center;
+  align-items:center;
+  height:30px;
+
+`;
+
+const Text = styled.Text`
+  color:white;
+  font-weight:bold;
+`;
+
+
 
 export default ({ navigation, route }) => {
 
-  const {photo} = route.params.photo;
-  console.log(route.params.photo);
+  const [loading, setLoading] = useState(false);
+  const [fileUrl, setFileUrl] = useState("");
+  const captionInput = useInput("");
+  const locationInput = useInput("");
+
+  const handleSubmmit = async () => {
+    if(captionInput.value === "" || locationInput === "") {
+      Alert.alert("Input need");
+    }
+  }
+
   return (
     <View>
-      <Text>Upload</Text>
+      <Container>
+        <Image
+          source={{ uri: route.params.photo.uri }}
+          style={{ height: 150, width: 150 }}
+        />
+        <Form>
+          <STextInput
+            onChangeText={captionInput.onChange}
+            value={captionInput.value}
+            placeholder="Caption"
+            placeholderTextColor={styles.darkGreyColor}
+          />
+          <STextInput
+            onChangeText={locationInput.onChange}
+            value={locationInput.value}
+            placeholder="Location"
+            placeholderTextColor={styles.darkGreyColor}
+          />
+          <Button onPress={()=> handleSubmmit()}>
+            <Text>Upload</Text>
+          </Button>
+        </Form>
+      </Container>
     </View>
   );
 };
