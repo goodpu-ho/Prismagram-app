6ac4d01d4e411fd7685b940631e806ad62ee520e
ho <whdxo10@naver.com>
2020-07-26 22:26:47 +0900

    07/26 APP: UPLOAD - Uploading to Backend
    	1. Yarn add axios
    	- Axios를 통해 backend로 image upload 
    	
    	2. Error로 3시간 고생.. Backend 작업시 /api/upload를 해야하는데 경로잘못씀;

--- apollo.js
+++ apollo.js
@@ -1,6 +1,7 @@
 const apolloClientOptions = {
 
-    uri: "http://3edcc87e6492.ngrok.io"
+    // uri: "http://a933c26943c4.ngrok.io"
+    uri: "http://localhost:4000"
 };
 
 export default apolloClientOptions;
--- screens/Photo/UploadPhoto.js
+++ screens/Photo/UploadPhoto.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import { Image, Alert } from "react-native";
+import axios from "axios";
+import Constants from "expo-constants";
 import useInput from "../../hooks/useInput";
 import styles from "../../styles";
 import constants from "../../constants";
@@ -16,45 +18,70 @@ const Container = styled.View`
 `;
 
 const Form = styled.View`
-  justify-content:space-between;
-  margin:10px;
-
+  justify-content: space-between;
+  margin: 10px;
 `;
 
 const STextInput = styled.TextInput`
   width: ${constants.width - 180};
   border: 0px solid ${styles.lightGreyColor};
-  border-bottom-width: 5px;     
+  border-bottom-width: 5px;
 `;
 
 const Button = styled.TouchableOpacity`
-  background-color:${styles.blueColor};
-  border-radius:4px;
-  justify-content:center;
-  align-items:center;
-  height:30px;
-
+  background-color: ${styles.blueColor};
+  border-radius: 4px;
+  justify-content: center;
+  align-items: center;
+  height: 30px;
 `;
 
 const Text = styled.Text`
-  color:white;
-  font-weight:bold;
+  color: white;
+  font-weight: bold;
 `;
 
-
-
 export default ({ navigation, route }) => {
-
   const [loading, setLoading] = useState(false);
   const [fileUrl, setFileUrl] = useState("");
   const captionInput = useInput("");
   const locationInput = useInput("");
+  const photo = route.params.photo;
 
   const handleSubmmit = async () => {
-    if(captionInput.value === "" || locationInput === "") {
+    if (captionInput.value === "" || locationInput === "") {
       Alert.alert("Input need");
     }
-  }
+
+    const formData = new FormData();
+    const name = photo.filename;
+    const [, type] = name.split(".");
+    
+    formData.append("file", {
+      name,
+      type: "image/jpeg",
+      uri: photo.uri,
+    });
+
+    // const { manifest } = Constants;
+    // const testuri = `http://${manifest.debuggerHost.split(':').shift()}:4000`;
+    // console.log(testuri);
+
+    
+    try {
+      const {
+        data: { path },
+      } = await axios.post("http://192.168.35.199:4000/api/upload", formData, {
+        headers: {          
+          "content-type": "multipart/form-data"
+        },
+      });
+      setFileUrl(path);
+    } catch (e) {
+      Alert.alert("Cant upload", "Try later");
+      console.log(e);
+    }
+  };
 
   return (
     <View>
@@ -76,7 +103,7 @@ export default ({ navigation, route }) => {
             placeholder="Location"
             placeholderTextColor={styles.darkGreyColor}
           />
-          <Button onPress={()=> handleSubmmit()}>
+          <Button onPress={() => handleSubmmit()}>
             <Text>Upload</Text>
           </Button>
         </Form>
